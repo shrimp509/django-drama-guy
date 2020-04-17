5a40e9424548b9429a8c493d3e7d353d24f721c7
RongSon <shrimp509@hotmail.com>
2020-04-17 13:48:55 +0800

    Add form of DramaRecord model

---
+++ record/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import DramaRecord
+
+
+class DramaRecordForm(forms.ModelForm):
+    class Meta:
+        model = DramaRecord
+        fields = {
+            'name',
+            'source',
+            'episode',
+            'max_episode',
+            'timestamp',
+        }
